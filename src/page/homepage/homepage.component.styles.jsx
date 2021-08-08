a5260a0366582d59f853a523552990a2844d3211
JLi <jli@equalityhealth.com>
2021-08-08 11:39:17 -0700

    minor style change

--- src/page/homepage/homepage.component.styles.jsx
+++ src/page/homepage/homepage.component.styles.jsx
@@ -9,19 +9,19 @@ export const NoteCollectionsContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 10px;
-  flex: 20%;
+  flex: 15%;
 `;
 
 export const NotesContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 10px;
-  flex: 20%;
+  margin: 10px 5px;
+  flex: 15%;
 `;
 
 export const NoteDetailContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 10px;
-  flex: 60%;
+  margin: 10px 0;
+  flex: 70%;
 `;
