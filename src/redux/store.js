c7bb9d5fdee0d417388cc8aa8041bace1374d089
JLi <jli@equalityhealth.com>
2021-08-07 18:45:20 -0700

    removed redux addon for heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,21 +11,27 @@ const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
 // force the ENV to staging for Heroku
-process.env.NODE_ENV = "staging";
+// process.env.NODE_ENV = "staging";
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
+// export const store = createStore(
+//   rootReducer,
+//   {},
+//   process.env.NODE_ENV === "development"
+//     ? compose(
+//         applyMiddleware(...middlewares),
+//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+//       )
+//     : applyMiddleware(...middlewares)
+// );
+
 export const store = createStore(
   rootReducer,
   {},
-  process.env.NODE_ENV === "development"
-    ? compose(
-        applyMiddleware(...middlewares),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-      )
-    : applyMiddleware(...middlewares)
+  applyMiddleware(...middlewares)
 );
 
 sagaMiddleware.run(rootSaga);
